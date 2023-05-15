172c19cc626e4e799bb19eb22bc7d5e1c0665c5c
HamidReza <delshad.swdn@gmail.com>
2023-05-15 17:15:52 +0200

    remove dead codes

--- client/app/todoList.js
+++ client/app/todoList.js
@@ -1,16 +1,16 @@
 "use client";
-import { useEffect, useState } from "react";
 import ToDoItem from "./todoItem";
 import styles from "./page.module.css";
 
-const ToDoList = ({tasks,fetchTasks}) => {
-
+const ToDoList = ({ tasks, fetchTasks }) => {
   return (
     <div>
       {tasks?.length === 0 && <p className={styles.ptag}>Please add a task!</p>}
       <div className={styles.todoList}>
         {tasks?.map((item, index) => {
-          return <ToDoItem key={index + 1} task={item} fetchTasks={fetchTasks}/>;
+          return (
+            <ToDoItem key={index + 1} task={item} fetchTasks={fetchTasks} />
+          );
         })}
       </div>
     </div>
